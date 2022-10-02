d21337649d54498818c812f7c2c2ce1f349d6494
Adrian Steyer <adrian.steyer@posteo.de>
2022-10-02 18:47:17 +0200

    file doubling fix?

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ def shape_info(info):
 
 
 def get_clan_output(clan):
+    os.remove("clans")
     runcmd('wget --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36" https://territorial.io/clans', verbose=True)
     info = get_info_clan(clan)
     if info != "error" and info != "not existing":
@@ -113,6 +114,7 @@ def get_clan_output(clan):
     
 
 def get_player_output(player):
+    os.remove("players")
     runcmd('wget --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36" https://territorial.io/players', verbose=True)
     info = get_info_player(player)
     if info != "error" and info != "not existing":
