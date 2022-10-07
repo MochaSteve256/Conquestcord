dbb329e86709745e6aee6163d33ad4cc25075863
Paxistan <n.schwichtenberg@aol.com>
2022-10-07 17:54:22 +0200

    lolaoallala

--- noaaaaah.py
+++ noaaaaah.py
@@ -23,7 +23,6 @@ for element in shaped_list:
             count_of_spaces += 1
             if count_of_spaces > 1:
                 values.append(temp)
-                print(values)
             
 
 
@@ -32,8 +31,8 @@ all_points = 0
 for value in values:
     try:
         all_points = all_points + float(value)
-        print(value)
+        print("Sucess", value)
     except Exception as e:
         print(e)
         pass
-print(int(all_points))
+print(all_points)
