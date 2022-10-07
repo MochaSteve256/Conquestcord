8f43ff8263d5254a49ed91fe9976fd9357c00432
Paxistan <n.schwichtenberg@aol.com>
2022-10-07 18:20:59 +0200

    Fixed all errors

--- noaaaaah.py
+++ noaaaaah.py
@@ -5,15 +5,24 @@ with open("clans", "r", encoding="utf-8") as f:
 shaped_list = []
 for line in content:
     line = line.replace("\n", "")
-    line = line.replace(",", "")
+    line = line + ","
     shaped_list.append(line)
 
 
 values = []
 for element in shaped_list:
-    position = element.find(".")
-    position -= 1
-    values.append(element[position:])
+    count_of_commata = 0
+    temp = ""
+    for char in element:
+        if char != ",":
+            temp = temp + char
+        else:
+            if count_of_commata > 1:
+                values.append(temp)
+                temp = ""
+            else:
+                temp = ""
+                count_of_commata += 1
 
 all_points = 0
 for value in values:
